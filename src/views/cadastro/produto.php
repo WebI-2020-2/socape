08eba6618f9aeef3a63d61e0b0c46a368a655bc4
jeffersonxavi <jefingbi2@gmail.com>
2021-08-04 19:53:35 -0300

    função apagar adicionada/ links corrigidos

--- src/views/cadastro/produto.php
+++ src/views/cadastro/produto.php
@@ -32,7 +32,7 @@
     <title>SOCAPE | Cadastrar Produto</title>
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
-    <link href="./../../../public/css/venda-parte1.css" rel="stylesheet">
+    <link href="./../../../public/css/cadastrar-fornecedor.css" rel="stylesheet">
     <script src="https://code.jquery.com/jquery-1.12.4.js"></script>  </head>
 
 <body>
