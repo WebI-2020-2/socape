f42f95c0bcc22f4789b378af74d961eade43982b
jeffersonxavi <jefingbi2@gmail.com>
2021-08-15 15:47:47 -0300

    removido tela cadastro

--- index.php
+++ index.php
@@ -22,26 +22,10 @@ $produtos = new ProdutosController();
 <body>
     <img src="./public/imagens/titulo.png">
     <nav class="navbar navbar-expand-lg navbar-black bg-black">
-        <div class="container-fluid">
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul style="width:100%;" class="navbar-nav me-auto mb-2 mb-lg-0">
+        <div class="collapse navbar-collapse">
+            <ul style="width:100%;" class="navbar-nav me-auto mb-2 mb-lg-0">
                     <li class="nav-item">
                         <a class="nav-link" style="color: #FFFFFF" href="./index.php">INÍCIO</a>
-                    </li>
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" style="color: #FFFFFF" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">CADASTRAR</a>
-                        <ul style="background-color: #140C0C " class="dropdown-menu" aria-labelledby="navbarDropdown">
-                            <li><a class="dropdown-item" style="color: #FFFFFF" href="src/views/cadastro/cliente-fisico.php">CLIENTE</a></li>
-                            <li><a class="dropdown-item" style="color: #FFFFFF" href="src/views/cadastro/fornecedor.php">FORNECEDOR</a></li>
-                            <li><a class="dropdown-item" style="color: #FFFFFF" href="src/views/cadastro/produto.php">PRODUTO</a></li>
-                            <li><a class="dropdown-item" style="color: #FFFFFF" href="src/views/cadastro/carro.php">CARRO</a></li>
-                            <li><a class="dropdown-item" style="color: #FFFFFF" href="src/views/cadastro/localizacao.php">LOCALIZAÇÃO</a></li>
-                            <li><a class="dropdown-item" style="color: #FFFFFF" href="src/views/cadastro/valvula.php">VÁLVULA</a></li>
-                            <li><a class="dropdown-item" style="color: #FFFFFF" href="src/views/cadastro/categoria.php">CATEGORIA</a></li>
-                            <li><a class="dropdown-item" style="color: #FFFFFF" href="src/views/cadastro/motor.php">MOTOR</a></li>
-                            <li><a class="dropdown-item" style="color: #FFFFFF" href="src/views/cadastro/anofabricacao.php">FABRICAÇÃO</a></li>
-                            <li><a class="dropdown-item" style="color: #FFFFFF" href="src/views/cadastro/marca.php">MARCA</a></li>
-                        </ul>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" style="color: #FFFFFF" href="src/views/venda/venda.php">VENDER</a>
@@ -50,24 +34,24 @@ $produtos = new ProdutosController();
                         <a class="nav-link" style="color: #FFFFFF" href="src/views/entrada/entrada.php">DAR ENTRADA</a>
                     </li>
                     <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" style="color: #FFFFFF" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">CONSULTAR</a>
-                        <ul style="background-color: #140C0C " class="dropdown-menu" aria-labelledby="navbarDropdown">
-                            <li><a class="dropdown-item" href="src/views/cadastro/cliente-fisico.php">CLIENTE</a></li>
-                            <li><a class="dropdown-item" href="src/views/cadastro/fornecedor.php">FORNECEDOR</a></li>
-                            <li><a class="dropdown-item" href="src/views/cadastro/produto.php">PRODUTO</a></li>
-                            <li><a class="dropdown-item" href="src/views/cadastro/carro.php">CARRO</a></li>
-                            <li><a class="dropdown-item" href="src/views/cadastro/localizacao.php">LOCALIZAÇÃO</a></li>
-                            <li><a class="dropdown-item" href="src/views/cadastro/valvula.php">VÁLVULA</a></li>
-                            <li><a class="dropdown-item" href="src/views/cadastro/categoria.php">CATEGORIA</a></li>
-                            <li><a class="dropdown-item" href="src/views/cadastro/motor.php">MOTOR</a></li>
-                            <li><a class="dropdown-item" href="src/views/cadastro/anofabricacao.php">FABRICAÇÃO</a></li>
-                            <li><a class="dropdown-item" href="src/views/cadastro/marca.php">MARCA</a></li>
-                        </ul>
-                    </li>
+                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">CONSULTAR</a>
+                    <ul class="dropdown-menu">
+                        <li><a class="dropdown-item" href="src/views/consulta/cliente.php">CLIENTE</a></li>
+                        <li><a class="dropdown-item" href="src/views/consulta/fornecedor.php">FORNECEDOR</a></li>
+                        <li><a class="dropdown-item" href="src/views/consulta/produto.php">PRODUTO</a></li>
+                        <li><a class="dropdown-item" href="src/views/consulta/carro.php">CARRO</a></li>
+                        <li><a class="dropdown-item" href="src/views/consulta/localizacao.php">LOCALIZAÇÃO</a></li>
+                        <li><a class="dropdown-item" href="src/views/consulta/valvula.php">VÁLVULA</a></li>
+                        <li><a class="dropdown-item" href="src/views/consulta/categoria.php">CATEGORIA</a></li>
+                        <li><a class="dropdown-item" href="src/views/consulta/motor.php">MOTOR</a></li>
+                        <li><a class="dropdown-item" href="src/views/consulta/anofabricacao.php">FABRICAÇÃO</a></li>
+                        <li><a class="dropdown-item" href="src/views/consulta/marca.php">MARCA</a></li>
+                    </ul>
+                </li>
                     <li class="nav-item">
                         <a class="nav-link" style="color: #FFFFFF" href="#">OLÁ <?= $_SESSION['nome']; ?></a>
                     </li>
-                    <li style="margin-left: 45%" class="nav-item dropdown">
+                    <li style="margin-left: 53%" class="nav-item dropdown">
                         <a class="nav-link dropdown-toggle" style="color: #FFFFFF" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">MINHA CONTA</a>
                         <ul style="background-color: #140C0C " class="dropdown-menu" aria-labelledby="navbarDropdown">
                             <li><a class="dropdown-item" style="color: #FFFFFF" href="src/views/usuario/perfil.php">PERFIL</a></li>
