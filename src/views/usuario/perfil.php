cb4b064c2785174f4a82889c87176feaf642279e
Marcos Coelho <kinho17socramrockrusso@gmail.com>
2021-08-17 15:25:31 -0300

    .

--- src/views/usuario/perfil.php
+++ src/views/usuario/perfil.php
@@ -9,7 +9,7 @@ session_start();
     <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>SOCAPE | Venda</title>
+    <title>SOCAPE | MINHA CONTA</title>
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
     <link href="./../../../public/css/estilos.css" rel="stylesheet">
